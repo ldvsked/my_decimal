20eb10fa73e20d35be02aeb38480738f3ccc4ec4
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-11-29 14:57:55 +0300

    add scale normalization func

--- src/helpers.c
+++ src/helpers.c
@@ -1,22 +1,42 @@
 #include "s21_decimal.h"
 
-int get_sign(s21_decimal value) {
-    return (value.bits[3] >> 31) & 1;
+int get_sign(s21_decimal value) { return (value.bits[3] >> 31) & 1; }
+
+void set_sign(s21_decimal* value, int sign) {
+  if (sign) {
+    value->bits[3] |= (1 << 31);
+  } else {
+    value->bits[3] &= ~(1 << 31);
+  }
 }
 
-void set_sign(s21_decimal *value, int sign) {
-    if (sign) {
-        value->bits[3] |= (1 << 31);
-    } else {
-        value->bits[3] &= ~(1 << 31);
+int get_scale(s21_decimal value) { return (value.bits[3] >> 16) & 0xFF; }
+
+void set_scale(s21_decimal* value, int scale) {
+  value->bits[3] &= 0x80000000;
+  value->bits[3] |= (scale << 16);
+}
+
+void normalize_scale(s21_decimal* value_1, s21_decimal* value_2) {
+  int scale1 = get_scale(*value_1);
+  int scale2 = get_scale(*value_2);
+  if (scale1 == scale2) return;
+  // приводим к большей степени умножением на 10
+  s21_decimal* smaller_scale = (scale1 < scale2) ? value_1 : value_2;
+  int diff = (scale1 < scale2) ? (scale2 - scale1) : (scale1 - scale2);
+  for (int i = 0; i < diff; i++) {
+    unsigned long long carry = 0;
+    for (int j = 0; j < 3; j++) {
+      unsigned long long temp =
+          (unsigned long long)(unsigned int)smaller_scale->bits[j] * 10ULL +
+          carry;
+      smaller_scale->bits[j] = (int)(temp & 0xFFFFFFFF);
+      carry = temp >> 32;
     }
-}
-
-int get_scale(s21_decimal value) {
-    return (value.bits[3] >> 16) & 0xFF;
-}
-
-void set_scale(s21_decimal *value, int scale) {
-    value->bits[3] &= 0x80000000;
-    value->bits[3] |= (scale << 16);
+  }
+  if (scale1 < scale2) {
+    set_scale(value_1, scale2);
+  } else {
+    set_scale(value_2, scale1);
+  }
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -10,6 +10,7 @@ int get_sign(s21_decimal value);
 void set_sign(s21_decimal *value, int sign);
 int get_scale(s21_decimal value);
 void set_scale(s21_decimal *value, int scale);
+void normalize_scale(s21_decimal *value_1, s21_decimal *value_2);
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
