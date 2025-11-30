7d06e454dbee50dce0a78a43caf6ebc5fb438c66
authentk <authentk@msk-school.21>
2025-11-30 22:05:34 +0300

    ALL

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,11 +1,9 @@
 #ifndef s21_decimal_h
 #define s21_decimal_h
 
-typedef struct 
-{
-    unsigned int bits[4];
+typedef struct {
+  unsigned int bits[4];
 } s21_decimal;
-
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
